b8cdb6d927931026b2a55b8726770f85d2cc0d07
Justin Pulley <justinpulley@Justins-MacBook-Pro.local>
2023-07-11 23:32:16 -0500

    build: project is deploying but delegate seems scuffed.

--- test/MyDelegate.t.sol
+++ test/MyDelegate.t.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.17;
 
 import "forge-std/Test.sol";
+import "forge-std/console.sol";
 import {MyDelegate} from "../src/MyDelegate.sol";
 import {MyDelegateProjectDeployer} from "../src/MyDelegateProjectDeployer.sol";
 import {IJBDelegatesRegistry} from "@jbx-protocol/juice-delegates-registry/src/interfaces/IJBDelegatesRegistry.sol";
@@ -9,147 +10,144 @@ import {IJBOperatorStore} from "@jbx-protocol/juice-contracts-v3/contracts/inter
 import {IJBFundingCycleBallot} from "@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBFundingCycleBallot.sol";
 import {JBGlobalFundingCycleMetadata} from "@jbx-protocol/juice-contracts-v3/contracts/structs/JBFundingCycleMetadata.sol";
 import {MyDelegateDeployer} from "./../src/MyDelegateDeployer.sol";
-import {MyDelegateProjectDeployer} from "./../src/MyDelegateProjectDeployer.sol";
 import {IDelegateProjectDeployer} from "../src/interfaces/IDelegateProjectDeployer.sol";
 import "@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBController3_1.sol";
 import "@jbx-protocol/juice-delegates-registry/src/JBDelegatesRegistry.sol";
+import "@jbx-protocol/juice-delegates-registry/src/interfaces/IJBDelegatesRegistry.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "../src/structs/LaunchProjectData.sol";
 import "../src/structs/LaunchFundingCyclesData.sol";
 import "../src/structs/DeployMyDelegateData.sol";
+import "./helpers/TestBaseWorkflowV3.sol";
 
-contract MyDelegateTest_Unit is Test {
+contract MyDelegateTest_Unit is TestBaseWorkflowV3 {
     using stdStorage for StdStorage;
+    using JBFundingCycleMetadataResolver for JBFundingCycle;
 
+    JBProjectMetadata _projectMetadata;
+    JBFundingCycleData _data;
+    JBFundingCycleData _dataReconfiguration;
+    JBFundingCycleData _dataWithoutBallot;
+    JBFundingCycleMetadata _metadata;
+    JBFundAccessConstraints[] _fundAccessConstraints; // Default empty
+    IJBPaymentTerminal[] _terminals; // Default empty
+    MyDelegate _delegateImpl;
+    MyDelegateDeployer _delegateDepl;
     JBDelegatesRegistry delegatesRegistry;
-    MyDelegateProjectDeployer dDeployer;
-    IDelegateProjectDeployer pDeployer;
-    MyDelegate _delegateImplementation;
+    DeployMyDelegateData delegateData;
+    MyDelegateProjectDeployer projectDepl;
 
-    address owner = address(bytes20(keccak256("owner")));
-    address reserveBeneficiary = address(bytes20(keccak256("reserveBeneficiary")));
-    address mockJBDirectory = address(bytes20(keccak256("mockJBDirectory")));
-    address mockTokenUriResolver = address(bytes20(keccak256("mockTokenUriResolver")));
-    address mockTerminalAddress = address(bytes20(keccak256("mockTerminalAddress")));
-    address mockJBController = address(bytes20(keccak256("mockJBController")));
-    address mockJBFundingCycleStore = address(bytes20(keccak256("mockJBFundingCycleStore")));
-    address mockJBOperatorStore = address(bytes20(keccak256("mockJBOperatorStore")));
-    address mockJBProjects = address(bytes20(keccak256("mockJBProjects")));
-    uint256 projectId = 69;
-    string fcMemo = "meemoo";
+    uint256 _projectId;
+    uint256 reservedRate = 4500;
+    uint256 weight = 10 ** 18; // Minting 1 token per eth
 
-    function setUp() public {
-        vm.label(owner, "owner");
-        
-        vm.label(mockTokenUriResolver, "mockTokenUriResolver");
-        vm.label(mockTerminalAddress, "mockTerminalAddress");
-        vm.label(mockJBController, "mockJBController");
-        vm.label(mockJBDirectory, "mockJBDirectory");
-        vm.label(mockJBFundingCycleStore, "mockJBFundingCycleStore");
-        vm.label(mockJBProjects, "mockJBProjects");
-        vm.etch(mockJBController, new bytes(0x69));
-        vm.etch(mockJBDirectory, new bytes(0x69));
-        vm.etch(mockJBFundingCycleStore, new bytes(0x69));
-        vm.etch(mockTokenUriResolver, new bytes(0x69));
-        vm.etch(mockTerminalAddress, new bytes(0x69));
-        vm.etch(mockJBProjects, new bytes(0x69));
+    function setUp() public override {
+        super.setUp();
 
-        _delegateImplementation = new MyDelegate();
-       
-        delegatesRegistry = new JBDelegatesRegistry(IJBDelegatesRegistry(mockJBDirectory));
+        delegatesRegistry = new JBDelegatesRegistry(IJBDelegatesRegistry(address(0)));
 
-        MyDelegateDeployer _delegateDeployer = new MyDelegateDeployer(_delegateImplementation, delegatesRegistry);
-        dDeployer = new MyDelegateProjectDeployer(
-              _delegateDeployer,
-              IJBOperatorStore(mockJBOperatorStore)
-            );
+        _delegateImpl = new MyDelegate();
 
-        pDeployer = IDelegateProjectDeployer(address(dDeployer));
-    }
+        _delegateDepl = new MyDelegateDeployer(_delegateImpl, delegatesRegistry);
 
-    function testLaunchProject(uint128 previousProjectId) public {
-        // MyDelegate _myDelegate = new MyDelegate();
+        _projectMetadata = JBProjectMetadata({content: "myIPFSHash", domain: 1});
 
-        vm.assume(previousProjectId < type(uint88).max);
+        projectDepl = new MyDelegateProjectDeployer(_delegateDepl, _jbOperatorStore);
 
-        (
-            DeployMyDelegateData memory delegateData,
-            LaunchProjectData memory launchProjectData
-        ) = createData();
-
-
-        vm.mockCall(mockJBDirectory, abi.encodeWithSelector(IJBDirectory.projects.selector), abi.encode(mockJBProjects));
-        vm.mockCall(mockJBProjects, abi.encodeWithSelector(IERC721.ownerOf.selector), abi.encode(owner));
-        vm.mockCall(mockJBProjects, abi.encodeWithSelector(IJBProjects.count.selector), abi.encode(previousProjectId));
-        vm.mockCall(mockJBController, abi.encodeWithSelector(IJBController3_1.launchProjectFor.selector), abi.encode(true));
-        uint256 _projectId = dDeployer.launchProjectFor(owner, delegateData, launchProjectData, IJBController3_1(mockJBController));
-
-        /* _pDeployer.launchProjectFor(address(0), ) */
-
-        assertEq(previousProjectId, _projectId - 1);
-    }
-
-    function createData()
-        internal
-        view
-        returns (
-            DeployMyDelegateData memory delegateData,
-            LaunchProjectData memory launchProjectData
-        )
-    {
-        address[] memory aList = new address[](1);
-        aList[0] = owner;
-
-        delegateData = DeployMyDelegateData({
-            allowList: aList
-        });
-
-        JBProjectMetadata memory projectMetadata;
-        JBFundingCycleData memory data;
-        JBFundingCycleMetadata memory metadata;
-        JBGroupedSplits[] memory groupedSplits;
-        JBFundAccessConstraints[] memory fundAccessConstraints;
-        IJBPaymentTerminal[] memory terminals;
-        projectMetadata = JBProjectMetadata({content: "myIPFSHash", domain: 1});
-        data = JBFundingCycleData({
-            duration: 14,
-            weight: 10 ** 18,
-            discountRate: 450000000,
+        _data = JBFundingCycleData({
+            duration: 6 days,
+            weight: weight,
+            discountRate: 0,
             ballot: IJBFundingCycleBallot(address(0))
         });
-        metadata = JBFundingCycleMetadata({
+
+        _metadata = JBFundingCycleMetadata({
             global: JBGlobalFundingCycleMetadata({
                 allowSetTerminals: false,
                 allowSetController: false,
                 pauseTransfers: false
             }),
-            reservedRate: 5000, //50%
-            redemptionRate: 5000, //50%
+            reservedRate: reservedRate,
+            redemptionRate: 5000,
             ballotRedemptionRate: 0,
             pausePay: false,
             pauseDistributions: false,
             pauseRedeem: false,
             pauseBurn: false,
-            allowMinting: false,
+            allowMinting: true,
+            preferClaimedTokenOverride: false,
             allowTerminalMigration: false,
             allowControllerMigration: false,
             holdFees: false,
-            preferClaimedTokenOverride: false,
             useTotalOverflowForRedemptions: false,
-            useDataSourceForPay: false,
+            useDataSourceForPay: true,
             useDataSourceForRedeem: false,
-            dataSource: address(_delegateImplementation),
-            metadata: 0x00
+            dataSource: address(_delegateImpl),
+            metadata: 0
         });
-        launchProjectData = LaunchProjectData({
-            projectMetadata: projectMetadata,
-            data: data,
-            metadata: metadata,
+
+        _fundAccessConstraints.push(
+            JBFundAccessConstraints({
+                terminal: _jbETHPaymentTerminal,
+                token: jbLibraries().ETHToken(),
+                distributionLimit: 2 ether,
+                overflowAllowance: type(uint232).max,
+                distributionLimitCurrency: 1, // Currency = ETH
+                overflowAllowanceCurrency: 1
+            })
+        );
+
+        _terminals = [_jbETHPaymentTerminal];
+
+        JBGroupedSplits[] memory _groupedSplits = new JBGroupedSplits[](1); // Default empty
+
+        address[] memory aList = new address[](1);
+        aList[0] = address(123);
+
+        delegateData = DeployMyDelegateData({
+            allowList: aList
+        });
+
+         LaunchProjectData memory launchProjectData = LaunchProjectData({
+            projectMetadata: _projectMetadata,
+            data: _data,
+            metadata: _metadata,
             mustStartAtOrAfter: 0,
-            groupedSplits: groupedSplits,
-            fundAccessConstraints: fundAccessConstraints,
-            terminals: terminals,
-            memo: fcMemo
+            groupedSplits: _groupedSplits,
+            fundAccessConstraints: _fundAccessConstraints,
+            terminals: _terminals,
+            memo: ""
         });
+
+        _projectId = projectDepl.launchProjectFor(
+            address(123),
+            delegateData,
+            launchProjectData,
+            _jbController
+        );
+
+    }
+
+    function testLaunchProject() public {
+        emit log_uint(_projectId);
+
+        vm.deal(address(123), 2 ether);
+        vm.deal(address(124), 2 ether);
+        vm.prank(address(123));
+        _jbETHPaymentTerminal.pay{value: 1 ether}(
+            1,
+            1,
+            address(0),
+            address(123),
+            /* _minReturnedTokens */
+            1,
+            /* _preferClaimedTokens */
+            false,
+            /* _memo */
+            "Take my money!",
+            /* _delegateMetadata */
+            ""
+        );
+
     }
 }
